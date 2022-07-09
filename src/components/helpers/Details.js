7ccae14ac9f2a40db978c48cefb9aae458dad3ba
EmilAvramov <e_avramov@zoho.eu>
2022-07-09 09:56:45 +0300

    added more functionality to Details

--- src/components/helpers/Details.js
+++ src/components/helpers/Details.js
@@ -1,25 +1,54 @@
+import { useState } from 'react';
 import styles from '../../styles/components/Details.module.scss';
 
-const Details = ({details, single, hasToken}) => {
-	console.log(single.genres[0].name)
-	const genres = []
-	const platforms = []
-	const companies = []
-	single.genres.forEach(el => genres.push(el.name))
-	single.platforms.forEach(el => platforms.push(el.abbreviation))
-	single.involved_companies.forEach(el => companies.push(el.company.name))
-	console.log(genres)
+const Details = ({ details, single, hasToken }) => {
+	const [links, toggleLinks] = useState(false);
+
 	const imgCover = single.cover.url.replace('t_thumb', 't_cover_big');
+	const genres = [];
+	const platforms = [];
+	const companies = [];
+	const urls = [];
+	const categories = {
+		1: 'Official',
+		3: 'Wikipedia',
+		10: 'iOS',
+		12: 'Android',
+		13: 'Steam',
+		17: 'GOG',
+	};
+	single.genres.forEach((el) => genres.push(el.name));
+	single.platforms.forEach((el) => platforms.push(el.abbreviation));
+	single.involved_companies.forEach((el) => companies.push(el.company.name));
+	single.websites.forEach((el) => {
+		if (
+			(el.category === 1 ||
+				el.category === 3 ||
+				el.category === 10 ||
+				el.category === 12 ||
+				el.category === 13 ||
+				el.category === 17)
+		) {
+			urls.push([categories[el.category], el.url]);
+		}
+	});
+
+	const handleLinks = () => {
+		toggleLinks((state) => !state);
+	};
+
 	return (
 		<div className={styles['details__container']}>
-            <button onClick={() => details()} className={styles['details__close']}>X</button>
+			<button
+				onClick={() => details()}
+				className={styles['details__close']}
+			>
+				X
+			</button>
 			<p className={styles['details__name']}>{single.name}</p>
 			<div className={styles['details__body']}>
-				<div className={styles['details__img']}>				
-					<img
-						src={imgCover}
-						alt=''
-					/>
+				<div className={styles['details__img']}>
+					<img src={imgCover} alt='' />
 				</div>
 				<div className={styles['details__info']}>
 					<div className={styles['details__subinfo']}>
@@ -27,7 +56,9 @@ const Details = ({details, single, hasToken}) => {
 							<p>{platforms.join(', ')} </p>
 						</div>
 						<div>
-							<p className={styles['star']}>{Math.floor(single.total_rating)}</p>
+							<p className={styles['star']}>
+								{Math.floor(single.total_rating)}
+							</p>
 						</div>
 					</div>
 					<p>Genres: {genres.join(', ')}</p>
@@ -39,7 +70,27 @@ const Details = ({details, single, hasToken}) => {
 			<div className={styles['details__buttons']}>
 				<button>Add to Library</button>
 				{!hasToken && <button>Remove from Library</button>}
-				<button>Visit Website</button>
+				<button
+					className={styles['details__linksHolder']}
+					onClick={handleLinks}
+				>
+					Links
+					{links && (
+						<ul className={styles['details__links']}>
+							{urls.map((el) => (
+								<li key={el[0]}>
+									<a	
+										href={el[1]}
+										target='_blank'
+										rel='noreferrer'
+									>
+										{el[0]}
+									</a>
+								</li>
+							))}
+						</ul>
+					)}
+				</button>
 				<button>Community</button>
 			</div>
 		</div>
--- src/services/queryCompiler.js
+++ src/services/queryCompiler.js
@@ -1,17 +1,17 @@
 const compileQuery = (data) => {
 	const setSearch = (val) => `search "${val}";`;
 	const setPlatformFilter = (val) =>
-		`where cover != null & platforms.abbreviation = "${val}";`;
+		`where cover != null & rating != null & platforms.abbreviation = "${val}";`;
 	const setCompanyFilter = (val) =>
-		`where cover != null & involved_companies.company.name ~ *"${val}"*;`;
+		`where cover != null & rating != null & involved_companies.company.name ~ *"${val}"*;`;
 
 	let fields =
-		'fields id, name, cover.url, genres.name, platforms.abbreviation, involved_companies.company.name, total_rating_count, total_rating, release_dates.human, summary, url;';
+		'fields id, name, cover.url, genres.name, platforms.abbreviation, involved_companies.company.name, total_rating_count, total_rating, release_dates.human, summary, websites.*;';
 	let limit = 'limit 500;';
-	let filter = `where cover != null & platforms.abbreviation = "PC";`;
+	let filter = `where cover != null & rating != null & platforms.abbreviation = "PC";`;
 	let search;
 
-	let query = `${fields} where cover != null & platforms.abbreviation = "PC"; limit 500;`;
+	let query = `${fields} where cover != null & rating != null & platforms.abbreviation = "PC"; limit 500;`;
 
 	if (data.length === 0) {
 		return query;
@@ -22,7 +22,7 @@ const compileQuery = (data) => {
 				if (data.platform) {
 					filter = setPlatformFilter(data.platform);
 				} else {
-					filter = `where cover != null & platforms.abbreviation = "PC";`;
+					filter = `where cover != null & rating != null & platforms.abbreviation = "PC";`;
 				}
 				query = `${search} ${fields} ${filter} ${limit}`;
 			} else {
@@ -40,7 +40,7 @@ const compileQuery = (data) => {
 				if (!data.platform) {
 					filter = setCompanyFilter(data.string);
 				} else {
-					filter = `where cover != null & platforms.abbreviation = "${data.platform}" & involved_companies.company.name ~ *"${data.string}"*;`;
+					filter = `where cover != null & rating != null & platforms.abbreviation = "${data.platform}" & involved_companies.company.name ~ *"${data.string}"*;`;
 				}
 			}
 			query = `${fields} ${filter} ${limit}`;
