cbe10780317b3c2158aec96bd06aa4104c1c7068
EmilAvramov <e_avramov@zoho.eu>
2022-07-27 13:04:39 +0300

    refactored home page to make it less coupled

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,18 +1,16 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-import HomeCarousel from './HomeCarousel';
 import useFetchUser from '../../hooks/useFetchUser';
 import { useAuth } from '../../contexts/AuthContext';
 import useGetAllData from '../../hooks/useGetAllData';
 import useGetAllGames from '../../hooks/useGetAllGames';
 
-import Err from '../helpers/Error';
-import Loader from '../helpers/GridLoader';
-import { homeLoader, topLoader } from '../../styles/auxilary/loaderStyles';
+import HomeMain from './HomeMain';
+import HomeTop from './HomeTop';
+import Loader from '../helpers/RingLoader';
+import { homeLoader } from '../../styles/auxilary/loaderStyles';
 
-import banner from '../../assets/guest-banner.png';
 import styles from '../../styles/components/Home.module.scss';
-import { useEffect } from 'react';
 
 const Home = () => {
 	// Call relevant hooks
@@ -35,74 +33,24 @@ const Home = () => {
 				currentUser ? styles['home__wrapper'] : styles['home__guest']
 			}
 		>
-			<div className={styles['home__top']}>
-				{currentUser ? (
-					userLoading ? (
-						<Loader
-							loading={userLoading}
-							styles={topLoader}
-							size={10}
-						/>
-					) : userError ? (
-						<Err error={userError} styles={'home__top'} />
-					) : (
-						<>Currently browsing as {name}</>
-					)
-				) : (
-					<>Currently browsing as Guest</>
-				)}
-			</div>
-
-			{currentUser ? (
-				<>
-					<main className={styles['home__main']}>
-						{fetchLoading ? (
-							<Loader
-								loading={fetchLoading}
-								styles={homeLoader}
-								size={30}
-							/>
-						) : fetchError ? (
-							<Err error={fetchError} />
-						) : (
-							<>
-								<h2>Most popular games among our users</h2>
-								<HomeCarousel data={gamesData} />
-							</>
-						)}
-					</main>
-					<aside className={styles['home__aside']}>
-						{dataLoading ? (
-							<Loader
-								loading={dataLoading}
-								styles={homeLoader}
-								size={30}
-							/>
-						) : dataError ? (
-							<Err error={dataError} styles={'home__aside'} />
-						) : (
-							<>
-								<div className={styles['home__container']}>
-									We have {userCount}{' '}
-									{userCount > 1 ? 'users.' : 'user.'}
-								</div>
-								<div className={styles['home__container']}>
-									Our users have added {gamesCount} games to
-									their libraries.
-								</div>
-								<div className={styles['home__container']}>
-									Our users have uploaded {screenshotCount}{' '}
-									screenshots.
-								</div>
-							</>
-						)}
-					</aside>
-				</>
+			{userLoading || fetchLoading || dataLoading ? (
+				<Loader loading={userLoading} styles={homeLoader} size={80} />
 			) : (
 				<>
-					<main className={styles['home__guest']}>
-						<img src={banner} alt='' />
-					</main>
+					<HomeTop
+						currentUser={currentUser}
+						userError={userError}
+						name={name}
+					/>
+					<HomeMain
+						currentUser={currentUser}
+						fetchError={fetchError}
+						gamesData={gamesData}
+						dataError={dataError}
+						userCount={userCount}
+						gamesCount={gamesCount}
+						screenshotCount={screenshotCount}
+					/>
 				</>
 			)}
 		</div>
